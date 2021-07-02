db0014354e93a75a90865d5bae94852e0ef8f0df
ehf <me@ehf.im>
2021-07-02 12:14:25 -0700

    Update get-ssl-cert-validity.sh

--- get-ssl-cert-validity.sh
+++ get-ssl-cert-validity.sh
@@ -2,10 +2,11 @@
 
 
 ilo_host="$1"
+ilo_host_ssl_port='443'
 
 test -x /usr/bin/openssl  || ( printf "openssl not available\n" && exit 1 )
 
-validity=$(echo '' | openssl s_client -host ${ilo_host} -port 443 2>/dev/null |  openssl x509 -noout -text | grep -A2 Validity | tail -n +2 | xargs | sed 's/Not After/,Not After/g')
+validity=$(echo '' | openssl s_client -host ${ilo_host} -port ${ilo_host_ssl_port} 2>/dev/null |  openssl x509 -noout -text | grep -A2 Validity | tail -n +2 | xargs | sed 's/Not After/,Not After/g')
 printf "${ilo_host},${validity}\n"
 
 exit 0
